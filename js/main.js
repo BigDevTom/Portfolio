2d8174cff082105ccdd7be5d0b4a7260a8f5f6cb
Thomas Colliot <thomas.colliot86300@gmail.com>
2025-06-10 11:00:41 +0200

    responsive + menu burger

--- js/main.js
+++ js/main.js
@@ -7,3 +7,10 @@ toggle.addEventListener('click', () => {
   body.classList.toggle('dark-mode');
   toggle.textContent = body.classList.contains('dark-mode') ? '☀️' : '🌙';
 });
+
+const burger = document.getElementById('burger-menu');
+const nav = document.querySelector('nav');
+
+burger.addEventListener('click', () => {
+  nav.classList.toggle('show');
+});
